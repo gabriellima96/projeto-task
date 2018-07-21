6f63cb5278005a0a24964510faf098410a599379
Unknown <gabriellima.silva96@gmail.com>
2018-07-21 18:57:06 -0300

    Adicionando função de add task

--- src/main/java/site/gabriellima/projetotask/controller/TaskController.java
+++ src/main/java/site/gabriellima/projetotask/controller/TaskController.java
@@ -1,13 +1,42 @@
 package site.gabriellima.projetotask.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import site.gabriellima.projetotask.model.Task;
+import site.gabriellima.projetotask.service.TaskService;
 
 @Controller
 public class TaskController {
 	
+	@Autowired
+	private TaskService service;
+	
 	@GetMapping(value = "/")
 	public String index() {
 		return "index";
+	}
+	
+	@GetMapping(value = "/add")
+	public String addTask(Task task) {
+		return "addOrUpdate";
+	}
+	
+	@PostMapping(value = "/add")
+	public ModelAndView addTask(@Valid Task task, BindingResult result, RedirectAttributes redirect) {
+		if (result.hasErrors()) {
+			return new ModelAndView("/add");
+		}
+		
+		service.save(task);
+		redirect.addFlashAttribute("notification", "Task added!");
+		return new ModelAndView("redirect:/");
 	}
 }
