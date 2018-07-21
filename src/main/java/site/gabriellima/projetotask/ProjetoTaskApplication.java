3f127ca2f0dc524f86edaa54351ce45da5aaf287
Unknown <gabriellima.silva96@gmail.com>
2018-07-21 17:40:14 -0300

    Criação do projeto

---
+++ src/main/java/site/gabriellima/projetotask/ProjetoTaskApplication.java
@@ -0,0 +1,12 @@
+package site.gabriellima.projetotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoTaskApplication.class, args);
+	}
+}
---
+++ src/test/java/site/gabriellima/projetotask/ProjetoTaskApplicationTests.java
@@ -0,0 +1,16 @@
+package site.gabriellima.projetotask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoTaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
