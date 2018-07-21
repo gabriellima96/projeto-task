5133db0c0eeb1a562558bce94047b13be2284b34
Unknown <gabriellima.silva96@gmail.com>
2018-07-21 19:11:59 -0300

    Adicionando função de deletar Task

--- src/main/java/site/gabriellima/projetotask/controller/TaskController.java
+++ src/main/java/site/gabriellima/projetotask/controller/TaskController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -25,6 +26,10 @@ public class TaskController {
 	@GetMapping(value = "/")
 	public String index(Model model	) {
 		List<Task> tasks = service.findAll();
+
+		if (tasks.size() == 0) {
+			tasks = null;
+		}
 		model.addAttribute("tasks", tasks);
 		return "index";
 	}
@@ -43,5 +48,12 @@ public class TaskController {
 		service.save(task);
 		redirect.addFlashAttribute("notification", "Task added!");
 		return new ModelAndView("redirect:/");
+	}
+	
+	@GetMapping(value = "/delete/{id}")
+	public String deleteTask(@PathVariable("id") Integer id, RedirectAttributes redirect) {
+		service.delete(id);
+		redirect.addFlashAttribute("notification", "Task deleted!");
+		return "redirect:/";
 	}
 }
