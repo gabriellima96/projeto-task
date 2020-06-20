6371bef1f62e1211322e6b34fec37d197450ed17
Gabriel Lima <gabriellima.silva96@gmail.com>
2020-06-20 19:07:17 -0300

    Update Spring and Java

--- src/test/java/site/gabriellima/projetotask/ProjetoTaskApplicationTests.java
+++ src/test/java/site/gabriellima/projetotask/ProjetoTaskApplicationTests.java
@@ -1,16 +1,13 @@
 package site.gabriellima.projetotask;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProjetoTaskApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
